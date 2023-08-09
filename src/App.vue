08b21978a44320ef9fca5bc7922f1c24d443113f
왕준영 <wangsun7@naver.com>
2023-08-09 13:36:02 +0900

    feat: open vidu 연동기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <div>
+      <button @click="test">
+        테스트 연결
+      </button>
+      <hr>
+    </div>
     <div v-if="matchStatus===MatchStatus.READY">
       <button @click="match">매칭</button>
     </div>
@@ -24,10 +30,46 @@
 
       <button @click="start">시작</button>
       <hr>
+      <div id="session" v-if="session">
+        <div id="session-header">
+          <h1 id="session-title">{{ memberId }}</h1>
+        </div>
 
-      <div id="gameResponses"></div>
-      <div>
-        <button @click="pick">good</button>
+        <div id="main-video">
+          <user-video v-if="subscribers.length>-1" :stream-manager="subscribers[0]" />
+        </div>
+
+        <div>
+          <user-video  v-if="subscribers.length>0" :stream-manager="subscribers[1]"/>
+        </div>
+
+
+
+      </div>
+
+      <div v-if="gameStatus===GameStatus.WAIT_GAME_START">
+        <p>잠시후 게임 시작</p>
+        <p>{{second}}</p>
+      </div>
+
+      <div v-if="gameStatus===GameStatus.PICK_TOPIC_TYPE">
+        <p>대분류 뽑기</p>
+        <p>{{second}}</p>
+      </div>
+
+      <div v-if="gameStatus===GameStatus.PICK_TOPIC_KEYWORD">
+        <p>소분류 뽑기</p>
+        <p>{{second}}</p>
+      </div>
+      <div v-if="gameStatus===GameStatus.PREPARE_PRESENT">
+        발표 준비
+        {{tellerToken}}
+        {{second}}
+        <button @click="skipPreparePresent">발표 준비 완료</button>
+      </div>
+      <div v-if="gameStatus===GameStatus.PRESENT">
+        <p>발표중</p>
+        {{second}}
       </div>
     </div>
 
@@ -37,11 +79,18 @@
 <script>
 
 import {MatchStatus} from "@/match-status";
+import {GameStatus} from "@/game-status";
 import * as Stomp from "webstomp-client";
+import UserVideo from "@/components/UserVideo.vue";
+import { OpenVidu } from "openvidu-browser";
 
 export default {
   name: 'App',
+  components: {UserVideo},
   computed: {
+    GameStatus() {
+      return GameStatus
+    },
     MatchStatus() {
       return MatchStatus
     }
@@ -56,9 +105,25 @@ export default {
       sessionId:"",
       memberToken:"",
       gameResponse:"",
+      gameStatus:"",
+      tellerToken:"",
+      gameMemberOrder:"",
+      topic:"",
+      OV: undefined,
+      session: undefined,
+      mainStreamManager: undefined,
+      publisher: undefined,
+      subscribers: [],
     }
   },
+  unmounted() {
+    this.leaveSession();
+  },
   methods: {
+    updateMainVideoStreamManager(stream) {
+      if (this.mainStreamManager === stream) return;
+      this.mainStreamManager = stream;
+    },
     match() {
       /**
        * Todo
@@ -67,7 +132,6 @@ export default {
 
       this.matchStatus = MatchStatus.MATCHING;
 
-      // process.env.VUE_APP_MATCH_ENDPOINT_URI 는 /endpoint
       this.client = Stomp.client(process.env.VUE_APP_MATCH_ENDPOINT_URL);
       this.client.connect(
           {},
@@ -135,20 +199,101 @@ export default {
       this.client.send(`/enter/${this.groupId}/${this.memberId}`);
     },
     start(){
+      this.OV = new OpenVidu();
+      this.session = this.OV.initSession();
+
+      this.session.on("streamCreated", ({stream}) => {
+        const subscriber = this.session.subscribe(stream);
+        this.subscribers.push(subscriber);
+        console.log("subscriber Token 시발아",this.getMemberTokenByStreamManager(subscriber));
+        // this.subscribers.push({
+        //   streamManager: subscriber,
+        //   memberToken: ""
+        // });
+        // this.subscribers.push(subscriber);
+      });
+
+      this.session.on("streamDestroyed", ({stream}) => {
+        const index = this.subscribers.indexOf(stream.streamManager, 0);
+        if (index >= 0) {
+          this.subscribers.splice(index, 1);
+        }
+      });
+
+      this.session.on("exception", ({ exception }) => {
+        console.warn(exception);
+      });
+
+      this.session.connect(this.memberToken, {
+        clientData: this.memberToken
+      }).then(() => {
+            let publisher = this.OV.initPublisher(undefined, {
+              audioSource: undefined, // The source of audio. If undefined default microphone
+              videoSource: undefined, // The source of video. If undefined default webcam
+              publishAudio: true, // Whether you want to start publishing with your audio unmuted or not
+              publishVideo: true, // Whether you want to start publishing with your video enabled or not
+              resolution: "640x480", // The resolution of your video
+              frameRate: 30, // The frame rate of your video
+              insertMode: "APPEND", // How the video is inserted in the target element 'video-container'
+              mirror: false, // Whether to mirror your local video or not
+            });
+
+            this.publisher = publisher;
+            this.subscribers.push(publisher);
+        this.session.publish(this.publisher);
+
+      })
+          .catch((error) => {
+            console.log("There was an error connecting to the session:", error.code, error.message);
+          });
+
       this.client.subscribe(
           `/topic/game/${this.sessionId}`,
           (frame)=>{
-            // let gameResponse = JSON.parse(frame.body);
-            let gameResponseString = frame.body;
-            document.getElementById("gameResponses").innerHTML += `<p>${gameResponseString}</p>`;
+            let gameResponse = JSON.parse(frame.body);
+            // let gameResponseString = frame.body;
+            this.gameStatus = gameResponse.gameStatus;
+            this.second = gameResponse.second;
+
+            if(gameResponse.gameMemberOrder){
+              this.gameMemberOrder = gameResponse.gameMemberOrder;
+              console.log(this.gameMemberOrder);
+            }
+
+            if(gameResponse.tellerToken){
+              this.tellerToken = gameResponse.tellerToken;
+            }
+
+            if(gameResponse.topic){
+              this.topic = gameResponse.topic;
+            }
           },
           ()=>{
 
-          }
+          },
       )
     },
     pick(){
-      this.client.send(`/pick/${this.sessionId}`,JSON.stringify({type:1,keyword:"삼행시"}));
+      this.client.send(`/pick/shuffle/${this.sessionId}`,JSON.stringify(
+          {
+            memberToken:this.memberToken,
+            type:1,
+            keyword:"삼행시"
+          }));
+    },
+    skipPreparePresent(){
+      this.client.send(`/skip/prepare/${this.sessionId}`);
+    },
+    leaveSession() {
+      if (this.session) this.session.disconnect();
+      this.session = undefined;
+      this.mainStreamManager = undefined;
+      this.publisher = undefined;
+      this.subscribers = [];
+      this.OV = undefined;
+    },
+    getMemberTokenByStreamManager(streamManager){
+      return JSON.parse(streamManager.stream.connection.data).clientData;
     },
   },
 }
---
+++ src/game-status.js
@@ -0,0 +1,11 @@
+const GameStatus={
+    WAIT_GAME_START : 1,
+    ORDER_GAMEMEMBER : 2,
+    PICK_TOPIC_TYPE : 3,
+    PICK_TOPIC_KEYWORD : 4,
+    PREPARE_PRESENT : 5,
+    PRESENT : 6,
+    REFLECT_RANK : 7
+}
+
+export {GameStatus};
